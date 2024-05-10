c68f365abf55b124fe2149bebfc363887911f029
Bry2003 <164503688+Bry2003@users.noreply.github.com>
2024-05-10 20:34:43 +0800

    Update autoPay.java

--- src/welp/autoPay.java
+++ src/welp/autoPay.java
@@ -1,15 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package welp;
 
-/**
- *
- * @author Admin
- */
-public class autoPay {
+package welp;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class autoPay extends JFrame {
+    public JTextField cardNumber1;
+    public JTextField expirationDate2;
+    public JTextField postalCode3;
+    public JTextField securityCode4;
+ 
     autoPay(){
-        System.out.println("Bryan");
+       this.setTitle("Auto Payments");
+        this.setSize(300, 200);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+
+        JPanel bry= new JPanel();
+        bry.setLayout(new GridLayout(5, 2));
+
+        JLabel cardNumber = new JLabel("Card Number:");
+        cardNumber1 = new JTextField();
+        JLabel expirationDate = new JLabel("Expiration Date:");
+        expirationDate2 = new JTextField();
+        JLabel postalCode = new JLabel("Billing Postal Code:");
+        postalCode3= new JTextField();
+        JLabel securityCode = new JLabel("Security Code:");
+        securityCode4 = new JTextField();
+
+        JButton process = new JButton("Process Payment");
+
+       bry.add(cardNumber);
+        bry.add(cardNumber1);
+        bry.add(expirationDate);
+        bry.add(expirationDate2);
+        bry.add(postalCode);
+        bry.add(postalCode3);
+        bry.add(securityCode);
+        bry.add(securityCode4);
+        bry.add(process);
+
+        process.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+        
+        String cardNumber = cardNumber1.getText();
+        String expirationDate = expirationDate2.getText();
+        String postalCode = postalCode3.getText();
+        String securityCode = securityCode4.getText();
+
+       
+    }
+});
+          
+       add(bry);
+        setVisible(true);
     }
 }
