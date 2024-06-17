cdf036feb6c01b4b3a9ab709f1aeaf89cc528a55
Joana Marie Naval <163404736+DinMarie@users.noreply.github.com>
2024-06-17 21:02:08 +0800

    Update deposit.java

--- src/welp/deposit.java
+++ src/welp/deposit.java
@@ -8,107 +8,144 @@ package welp;
  *
  * @author Admin
  */
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class deposit extends JFrame{
+public class deposit extends JFrame implements ActionListener{
+
+    private JButton rj = new JButton("Transfer Money");
+    private JButton ko = new JButton("Next");
+    private JButton jk1 = new JButton("Cancel");
+    private JButton t2 = new JButton("Exit");
  
     deposit(){
-        JOptionPane.showMessageDialog(null, "Welcom to the Deposit System!", "WELCOME GREETING!",JOptionPane.INFORMATION_MESSAGE);
-        JPanel jm = new JPanel();
-
-        jm.setBackground(new Color(0x123456));
-        jm.setLayout(new GridLayout(7,1,50,20));
-        jm.setBounds(70,150,600,400);
-
-        JLabel jk = new JLabel("DEPOSIT");
-        jk.setBounds(100,10,200,150);
-        jk.setForeground(Color.white);
-        jk.setFont(new Font("Georgia",Font.BOLD,25));
-
-        JLabel jn1 = new JLabel("UserID: ");
-        jn1.setForeground(Color.white);
-        jn1.setFont(new Font("Arial Black",Font.BOLD,15));
-
-        JTextField yg1 = new JTextField();
-        JButton jh1 = new JButton("Search");
-
-        JLabel jn2 = new JLabel("Name: ");
-        jn2.setForeground(Color.white);
-        jn2.setFont(new Font("Arial Black",Font.BOLD,15));
-        JTextField yg2 = new JTextField();
-        JButton jh2 = new JButton("Enter");
-
-        JLabel jn3 = new JLabel("Debit Account: ");
-        jn3.setPreferredSize(new Dimension (100,50));
-        jn3.setForeground(Color.white);
-        jn3.setFont(new Font("Arial Black",Font.BOLD,15));  
-        JTextField yg3 = new JTextField();
-
-        JLabel nj3 = new JLabel("             Show       ");
-        nj3.setForeground(Color.white);
-        nj3.setFont(new Font("Arial Black",Font.BOLD,15));
-
-        JLabel jn4 = new JLabel("Available Balance:");
-        jn4.setForeground(Color.white);
-        jn4.setFont(new Font("Arial Black",Font.BOLD,15));
-        JTextField yg4 = new JTextField();
-        JButton jh4 = new JButton("OK");
-
-        JLabel jn5 = new JLabel("Amount");
-        jn5.setForeground(Color.white);
-        jn5.setFont(new Font("Arial Black",Font.BOLD,15));
-        JTextField yg5 = new JTextField();
-        JButton jh5 = new JButton("Enter");
-
-        JLabel jn6 = new JLabel("Acredit Account:");
-        jn6.setForeground(Color.white);
-        jn6.setFont(new Font("Arial Black",Font.BOLD,15));
-        JTextField yg6 = new JTextField();
-        JButton jh6 = new JButton("Enter");
-
-        JButton sty = new JButton("Exit");
-        sty.setBounds(100,510,100,20);
-
-        JButton ytc = new JButton("Next");
-        ytc.setBounds(100,540,100,20);
-
-        JFrame moon = new JFrame();
-        moon.setTitle("BANK ACCOUNT MANAGEMENT SYSTEM"); // title of frame
-        moon.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        moon.setResizable(false);
-        moon.setSize(750,650);
-        moon.setLocationRelativeTo(null);
-        moon.getContentPane().setBackground(new Color(0x123456));   
-        moon.setLayout(null);  
-        moon.setVisible(true);
-
-        jm.add(jn1);
-        jm.add(yg1);
-        jm.add(jh1);
-
-        jm.add(jn2);
-        jm.add(yg2);
-        jm.add(jh2);
-
-        jm.add(jn3);
-        jm.add(yg3);
-        jm.add(nj3);
-
-        jm.add(jn4);
-        jm.add(yg4);
-        jm.add(jh4);
-
-        jm.add(jn5);
-        jm.add(yg5);
-        jm.add(jh5);
-
-        jm.add(jn6);
-        jm.add(yg6);
-        jm.add(jh6); 
-        moon.add(jm);
-        moon.add(jk);
-        moon.add(sty);
-        moon.add(ytc);
+        ImageIcon iconic = new ImageIcon("capture.png");
+        JOptionPane.showMessageDialog(null, "Welcome to the Deposit System!", "WELCOME GREETING!",JOptionPane.INFORMATION_MESSAGE,iconic);
+         
+        JPanel jn = new JPanel();
+         jn.setLayout(new GridLayout(5,2,50,20));
+         jn.setBounds(70,150,700,300);
+        
+         JLabel sg = new JLabel("BANK DEPOSIT SYSTEM");
+         sg.setBounds(200,5,350,150);
+         sg.setForeground(Color.BLACK);
+         sg.setFont(new Font("Georgia",Font.BOLD,25));
+         
+         JLabel nj = new JLabel("Account Number: ");
+         nj.setForeground(Color.BLACK);
+         nj.setFont(new Font("Arial Black",Font.BOLD,15));
+          
+         JTextField nj1 = new JTextField();
+         JButton shot = new JButton("Search");
+        
+         JLabel jh = new JLabel("Account Name: ");
+         jh.setForeground(Color.BLACK);
+         jh.setFont(new Font("Arial Black",Font.BOLD,15));
+         
+         JTextField jh1 = new JTextField();
+         JButton mang = new JButton("Enter");
+         
+         
+         JLabel jm = new JLabel("Credit Balance: ");
+         jm.setPreferredSize(new Dimension (100,50));
+         jm.setForeground(Color.black);
+         jm.setFont(new Font("Arial Black",Font.BOLD,15));  
+         JTextField jm1= new JTextField();
+         
+         JLabel th = new JLabel("             Show       ");
+         th.setForeground(Color.BLACK);
+         th.setFont(new Font("Arial Black",Font.BOLD,15));
+         
+         JLabel jk = new JLabel("Deposit Amount");
+         jk.setForeground(Color.BLACK);
+         jk.setFont(new Font("Arial Black",Font.BOLD,15));
+         JTextField yg5 = new JTextField();
+         JButton jh5 = new JButton("Deposit");
+     
+         JLabel chim = new JLabel("Deposit Date:");
+         chim.setForeground(Color.BLACK);
+         chim.setFont(new Font("Arial Black",Font.BOLD,15));
+         JTextField yg6 = new JTextField();
+         JButton jh6 = new JButton("Enter");
+         
+         
+         jk1.setBounds(50, 500, 150, 40);
+         jk1.addActionListener(this);
+        
+         t2.setBounds(340,500,160,40);
+         t2.addActionListener(this);
+         
+         rj.setBounds(600,15,150,28);
+         rj.addActionListener(this);
+         
+         ko.setBounds(580,500,170,40);
+         ko.addActionListener(this);
+        
+        setTitle("BANK MANAGEMENT SYSTEM");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(840,650);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setVisible(true);
+        
+        add(jn);
+        jn.add(nj);
+        jn.add(nj1);
+        jn.add(shot);
+        jn.add(jh);
+        jn.add(jh1);
+        jn.add(mang);
+        
+        jn.add(jm);
+        jn.add(jm1);
+        jn.add(th);
+        
+        jn.add(jk);
+        jn.add(yg5);
+        jn.add(jh5);
+        
+        jn.add(chim);
+        jn.add(yg6);
+        jn.add(jh6);
+        
+        add(jk1);
+        add(t2);
+        add(rj);
+        add(ko);
+        add(sg);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    dispose();
+    if(e.getSource() == rj){
+        Transfer nana = new Transfer();
+       nana.setVisible(true);
+    }   
+    else if (e.getSource() == ko){
+        Next ne = new Next();
+        ne.setVisible(true);
+    }
+    else if (e.getSource() == jk1){
+    
+       System.out.println("Cancel The Process");
+    }
+    else if (e.getSource() == t2){
+      Exit lala = new Exit();
+      lala.setVisible(true);
+    }
+    
+   }
 }
