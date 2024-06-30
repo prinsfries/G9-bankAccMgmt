8f416f4528596fe5266c81cbc73b1683bce44a69
prinsfries <162795069+prinsfries@users.noreply.github.com>
2024-06-30 22:49:10 +0800

    Update Welp.java

--- src/welp/Welp.java
+++ src/welp/Welp.java
@@ -1,21 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package welp;
 
-
-/**
- *
- * @author Admin
- */
 public class Welp{
-    /**
-     * @param args the command line arguments
-     * @throws java.lang.InterruptedException
-     */
     public static void main(String[] args){
-        LogIn login = new LogIn();
-        System.out.println("");
+        
+        new LogIn();
     }
 }
