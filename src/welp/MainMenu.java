72ef43247b198aa040230785dc1c1ca06865a5f6
prinsfries <162795069+prinsfries@users.noreply.github.com>
2024-07-06 21:55:07 +0800

    Update MainMenu.java

--- src/welp/MainMenu.java
+++ src/welp/MainMenu.java
@@ -1,3 +1,4 @@
+
 package welp;
 
 import java.awt.*;
@@ -6,26 +7,24 @@ import javax.swing.*;
 
 public class MainMenu implements ActionListener{
     private static JFrame frame;    
-    private static JLabel mainMenu;
-    private static JButton withdraw, deposit, transac, pay, balance, transfer;
+    private static JLabel mainMenu, welcome;
+    private static JButton withdraw, deposit, transac, logout, balance, transfer;
     private static JPanel panel, title;
-    private static String u, p;
+    private static String u, p, n;
     private static int m;
     
-    MainMenu(String un, String pin, int money){
-        u = un; //Setting Parameters
+    MainMenu(String un, String pin, String an, int money){
+        u = un;
         p = pin;
         m = money;
-
-        //S.out logs for debug
+        n = an;
+        
         System.out.println("main menu");
         System.out.println(un);
         System.out.println(pin);
         System.out.println(money);
-        
-        
+        System.out.println(an);
         panel = new JPanel();
-        //JFrame
         frame = new JFrame("BANK ACCOUNT MANAGEMENT");
         frame.setSize(620,420);
         frame.setResizable(false);
@@ -33,63 +32,62 @@ public class MainMenu implements ActionListener{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         
-        //Panel elements
+        
         panel.setLayout(new GridLayout(5,1));
         panel.setPreferredSize(new Dimension(250,200));
-
-        //title
+        //panel.setBackground(Color.red);
+        
         title = new JPanel(new BorderLayout());
-
-        //main menu
+        //title.setPreferredSize(new Dimension(50,200));
+        
         mainMenu = new JLabel("MAIN MENU", SwingConstants.CENTER);
         mainMenu.setFont(new Font("Arial", Font.BOLD, 48));
         mainMenu.setBounds(10, 20, 250, 25);
         title.add(mainMenu);
         
         
-        //withdraw button
+        
         withdraw = new JButton ("Withdraw");
         withdraw.setFont(new Font("Arial", Font.BOLD, 24));
         withdraw.setBounds(10, 150, 150, 25);
         withdraw.addActionListener(this);
         panel.add(withdraw);
-
-        //deposit button
+        
         deposit = new JButton ("Deposit");
         deposit.setFont(new Font("Arial", Font.BOLD, 24));
         deposit.setBounds(10, 200, 150, 25);
         deposit.addActionListener(this);
         panel.add(deposit);
-
-        //transactions button
+        
         transac = new JButton ("Transactions");
         transac.setFont(new Font("Arial", Font.BOLD, 24));
         transac.setBounds(10, 200, 150, 25);
         transac.addActionListener(this);
         panel.add(transac);
-
-        //Auto Payments button
-        pay = new JButton ("Auto Payments");
-        pay.setFont(new Font("Arial", Font.BOLD, 24));
-        pay.setBounds(10, 200, 150, 25);
-        pay.addActionListener(this);
-        panel.add(pay);
-
-        //Balance tracking button
-        balance = new JButton ("Balances");
-        balance.setFont(new Font("Arial", Font.BOLD, 24));
-        balance.setBounds(10, 200, 150, 25);
-        balance.addActionListener(this);
-        panel.add(balance);
-
-        //Transfer money button
+        
         transfer = new JButton ("Transfer");
         transfer.setFont(new Font("Arial", Font.BOLD, 24));
         transfer.setBounds(10, 200, 150, 25);
         transfer.addActionListener(this);
         panel.add(transfer);
-
-        //add elements to frame
+        
+        balance = new JButton ("Balances");
+        balance.setFont(new Font("Arial", Font.BOLD, 24));
+        balance.setBounds(10, 200, 150, 25);
+        balance.addActionListener(this);
+        panel.add(balance);
+        
+        logout = new JButton ("Log out");
+        logout.setFont(new Font("Arial", Font.BOLD, 24));
+        logout.setBounds(10, 200, 150, 25);
+        logout.addActionListener(this);
+        panel.add(logout);
+        
+        welcome = new JLabel("Welcome, "+u+"!", SwingConstants.CENTER);
+        welcome.setFont(new Font("Arial", Font.BOLD, 24));
+        welcome.setBounds(10, 20, 250, 25);
+        panel.add(welcome);
+        
         frame.add(panel);
         frame.add(title,BorderLayout.NORTH);
         frame.setVisible(true);
@@ -98,31 +96,29 @@ public class MainMenu implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-//different outcomes of events
           if (e.getSource()==withdraw){
             frame.dispose();
-            withdraw wd = new withdraw();
+            withdraw wd = new withdraw(u,p,n,m);
             }
           else if (e.getSource()==deposit){
             frame.dispose();
-            deposit dp = new deposit(u,p,m);
+            deposit dp = new deposit(u,p,n,m);
             }
           else if (e.getSource()==transac){
             frame.dispose();
-            accTransac at = new accTransac(u);
+            accTransac at = new accTransac(u,p,n,m);
             }
-          else if (e.getSource()==pay){
+          else if (e.getSource()==logout){
             frame.dispose();
-            autoPay ap = new autoPay(u, p);
+            new LogIn();
             }
           else if (e.getSource()==balance){
             frame.dispose();
-            new tracking(u);
+            new tracking(u,p,n,m);
             }
           else if (e.getSource()==transfer){
             frame.dispose();
-            new transfer();
+            new transfer(u,p,n,m);
             }
     }
 }
